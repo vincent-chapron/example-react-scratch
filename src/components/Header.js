8ad7731d2c758b0e819a625da3ff7d545f752e8e
Vincent CHAPRON <vincent@chapron.io>
2017-10-11 14:39:33 +0200

    Add the first component to my application

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+  return React.createElement(
+    'header',
+    { className: 'header' },
+    React.createElement('h1', null, 'Example : React First Component'),
+  )
+}
