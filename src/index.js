800989c5e88bda3c6df556a30d5a798fc1d20f77
Vincent CHAPRON <vincent@chapron.io>
2017-10-11 15:19:58 +0200

    Add footer component, to show class based component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 ReactDOM.render(
-  React.createElement(Header),
+  React.createElement(
+    'div',
+    null,
+    React.createElement(Header),
+    React.createElement(Footer),
+  ),
   document.getElementById('root'),
 )
