865eebfca1d8d4bfad1f32c72e7080af745d6159
Vincent CHAPRON <vincent@chapron.io>
2017-10-16 12:31:13 +0200

    Add artivle component with children and title properties

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Header from './components/Header'
+import Article from './components/Article'
 import Footer from './components/Footer'
 
 ReactDOM.render(
   <div>
     <Header />
+    <Article title="Mon super titre">
+      <div>Lorem ipsum dolor sit amet.</div>
+    </Article>
     <Footer />
   </div>,
   document.getElementById('root'),
