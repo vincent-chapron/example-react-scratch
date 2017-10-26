6c3792f71738a0f51c5b7059447781aada51bd85
Vincent CHAPRON <vincent@chapron.io>
2017-10-26 16:44:46 +0200

    Add newsletter subscibe form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,23 @@ export default class Footer extends Component {
   constructor() {
     super()
     this.footer = 'This is the footer component'
+    this.state = { email: '' }
   }
 
   render() {
     return (
       <footer>
         <h4>{this.footer}</h4>
+        <form onSubmit={e => {
+          e.preventDefault()
+          console.log(this.state.email)
+        }}>
+          <input
+            type="email"
+            value={this.state.email}
+            onChange={e => this.setState({email: e.target.value})} />
+          <input type="submit" value="S'inscrire au newsletter !"/>
+        </form>
       </footer>
     )
   }
