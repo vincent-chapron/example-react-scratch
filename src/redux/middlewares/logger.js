3177e1b6ae09d757fbf30c1563baaead0dee8026
Vincent CHAPRON <vincent@chapron.io>
2017-10-26 14:32:55 +0200

    create logger middleware

---
+++ src/redux/middlewares/logger.js
@@ -0,0 +1,9 @@
+export default store => next => action => {
+    console.group(action.type)
+    console.info('initial state', store.getState())
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+}
